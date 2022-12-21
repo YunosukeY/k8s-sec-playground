d42fcd541885a05538460f0472696b482b2d4b6c
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2022-12-21 21:01:48 +0900

    feat: deploy cert-secret with eso

--- kind/e2e.sh
+++ kind/e2e.sh
@@ -36,8 +36,6 @@ deploy () {
   sleep 1 # hack
   kubectl apply -f https://raw.githubusercontent.com/YunosukeY/policies-for-pss/master/k8s/constraint_PodSecurityStandards.yaml -f "${repo_dir}/policy/constraint.yaml"
 
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout test.key -out test.crt -subj "/CN=example.com/O=example.com" -addext "subjectAltName = DNS:example.com"
-  kubectl create secret tls cert-secret --key test.key --cert test.crt
   helmfile apply -f "${repo_dir}/k8s/charts" -e $1
   kubectl wait --for condition=available deployment/ingress-nginx-controller --namespace=ingress --timeout=300s
   kubectl wait --for condition=available deployment/external-secrets-cert-controller deployment/external-secrets-webhook --namespace=kube-system --timeout=300s
