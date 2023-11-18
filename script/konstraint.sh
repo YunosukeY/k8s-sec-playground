00fa2dbe37857a4ef3dd46c632cbba5579a0f006
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2023-11-19 08:29:25 +0900

    update (#570)

--- script/konstraint.sh
+++ script/konstraint.sh
@@ -4,7 +4,7 @@ set -eu
 
 os="$(uname -s)"
 machine=$([[ $(uname -m) == arm64 ]] && echo arm64 || echo amd64)
-version="v0.24.0"
+version="v0.31.0"
 repo_dir="$(git rev-parse --show-toplevel)"
 bin="${repo_dir}/bin"
 konstraint="${bin}/konstraint"
